6e0bdf5ae1d8e28a8f536dc31d50dff6f121cf5a
Cixtor <cixtords@gmail.com>
2013-11-13 10:36:50 -0500

    Added. Simple tests to check sqlite support

--- app.js
+++ app.js
@@ -4,12 +4,12 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
 var app = express();
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('public/history.db');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -25,12 +25,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', function(req, res){
+    db.serialize(function(){
+        db.run('CREATE TABLE IF NOT EXISTS lorem (info TEXT)');
+        var stmt = db.prepare('INSERT INTO lorem VALUES (?)');
+        for( var i=0; i<10; i++ ){
+                stmt.run("Ipsum " + i);
+        }
+        stmt.finalize();
+        db.each('SELECT rowid AS id, info FROM lorem', function(err, row){
+                console.log(row.id + ': ' + row.info);
+        });
+        res.render('index', { title: 'Express' });
+    });
+});
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
+    // db.close();
 });
